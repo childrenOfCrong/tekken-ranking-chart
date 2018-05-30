ce335df49b99f5defd0d57e9eb4bd2439bafe55d
chany <lyhy0310@gmail.com>
2018-05-30 23:04:43 +0900

    [add] test dummy add

--- tekken-ranking-chart/src/js/tableView.js
+++ tekken-ranking-chart/src/js/tableView.js
@@ -1,5 +1,18 @@
-class tableView {
+class TableView {
   constructor(selector){
     this.table = document.querySelector(selector)
   }
+  orderByRank(){
+    const tableRows = this.table.querySelectorAll('tr')
+    tableRows.sort((a,b)=> {
+      const rateElA = a.document.querySelector('.rate-count')
+      const rateElB = a.document.querySelector('.rate-count')
+      return Number(rateElA.innerHTML)>Number(rateElB.innerHTML)
+    })
+    this.table.innerHTML = tableRows;
+  }
 }
+
+
+
+const tableView = new TableView('.table')
