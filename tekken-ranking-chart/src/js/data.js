a619892688a7de31fc3808e5e8c5d87a83a3434b
Dongwoo Lee <likedemian@me.com>
2018-06-03 03:26:36 +0900

    add: DOM Move by rates

--- tekken-ranking-chart/src/js/data.js
+++ tekken-ranking-chart/src/js/data.js
@@ -1,38 +1,42 @@
 const playersList = [
-  {"id": 1, "name": "달리", "win": 0, "lose": 0},
-  {"id": 2, "name": "만도", "win": 0, "lose": 0},
-  {"id": 3, "name": "우기", "win": 0, "lose": 0},
-  {"id": 4, "name": "웨일", "win": 0, "lose": 0},
-  {"id": 5, "name": "리오", "win": 0, "lose": 0},
-  {"id": 6, "name": "동동", "win": 0, "lose": 0},
-  {"id": 7, "name": "솔로몬", "win": 0, "lose": 0},
-  {"id": 8, "name": "루미", "win": 0, "lose": 0},
-  {"id": 9, "name": "새라", "win": 0, "lose": 0},
-  {"id": 10, "name": "현", "win": 0, "lose": 0},
-  {"id": 11, "name": "크롱", "win": 0, "lose": 0},
-  {"id": 12, "name": "호눅스", "win":0, "lose": 0},
-  {"id": 13, "name": "팀", "win": 0, "lose": 0},
+  { "id": 1, "name": "달리", "win": 0, "lose": 0 },
+  { "id": 2, "name": "만도", "win": 0, "lose": 0 },
+  { "id": 3, "name": "우기", "win": 0, "lose": 0 },
+  { "id": 4, "name": "웨일", "win": 0, "lose": 0 },
+  { "id": 5, "name": "리오", "win": 0, "lose": 0 },
+  { "id": 6, "name": "동동", "win": 0, "lose": 0 },
+  { "id": 7, "name": "솔로몬", "win": 0, "lose": 0 },
+  { "id": 8, "name": "루미", "win": 0, "lose": 0 },
+  { "id": 9, "name": "새라", "win": 0, "lose": 0 },
+  { "id": 10, "name": "현", "win": 0, "lose": 0 },
+  { "id": 11, "name": "크롱", "win": 0, "lose": 0 },
+  { "id": 12, "name": "호눅스", "win": 0, "lose": 0 },
+  { "id": 13, "name": "팀", "win": 0, "lose": 0 },
 ]
 
 class GameResults {
-  constructor(playersList){
+  constructor(playersList) {
     this.playersList = playersList;
-    this.gameResults = this.makeResults(this.playersList);
+    this.gameResults = this.makeResults(this.copyData());
   }
-  makeResults(playersList){
-    return this.playersList.reduce((ac,c)=>{
-       ac[c.id] = {}
-       ac[c.id].id = c.id;
-       ac[c.id].name = c.name;
-       ac[c.id].win = c.win;
-       ac[c.id].lose = c.lose;
-       ac[c.id].count = c.win+c.lose;
-       if(ac[c.id].count) ac[c.id].rate = ((c.win / ac[c.id].count)* 100).toFixed(0) + "%"
-       else ac[c.id].rate = 0 + "%";
-       return ac;
-    },{})
+
+  copyData() {
+    return this.playersList.slice();
+  }
+
+
+  makeResults(playersList) {
+    return this.playersList.reduce((acc, crr) => {
+      acc[crr.id] = {}
+      acc[crr.id].id = crr.id;
+      acc[crr.id].name = crr.name;
+      acc[crr.id].win = 0;
+      acc[crr.id].lose = 0;
+      acc[crr.id].count = 0;
+      acc[crr.id].rate = 0;
+      acc[crr.id].rank = 0;
+      return acc;
+    }, {})
   }
 }
 const gameResults = new GameResults(playersList)
-
-console.log(gameResults.gameResults);
--- tekken-ranking-chart/src/js/template.js
+++ tekken-ranking-chart/src/js/template.js
@@ -1,76 +1,156 @@
-let playerStates = document.querySelector(".player-states");
-
+let playerStates = document.querySelector('.player-states');
 const gameResultList = Object.values(gameResults.gameResults);
+
+
+
+
+
 function showItems(val, templateId) {
-  return val.reduce((acc, curr) => (acc += template(templateId, curr)), "");
+  return val.reduce((acc, curr) => (acc += template(templateId, curr)), '');
 }
+
+
+
+
 
 function template(templateid, data) {
   return document
-    .getElementById(templateid)
-    .innerHTML.replace(/{{(\w*)}}/g, (match, key) => (data.hasOwnProperty(key) ? data[key] : ""));
+    .getElementById(templateid).innerHTML
+    .replace(/{{(\w*)}}/g, (match, key) => (data.hasOwnProperty(key) ? data[key] : ''));
 }
 
-playerStates.innerHTML += showItems(gameResultList, "player-template");
+playerStates.innerHTML += showItems(gameResultList, 'player-template');
+
+
+
+
 
 const winBtn = document.querySelectorAll(`[id*=win-btn-]`);
 const loseBtn = document.querySelectorAll(`[id*=lose-btn-]`);
 console.log(winBtn, loseBtn);
 
+
+
+
+
+
 winBtn.forEach(elem => {
-  elem.addEventListener("click", e => {
+  elem.addEventListener('click', e => {
     const button = e.target;
-    const getId = button.id.split("-")[2];
+    const getId = button.id.split('-')[2];
     gameResults.gameResults[getId].win++;
     gameResults.gameResults[getId].count++;
+    gameResults.gameResults[getId].rate = Number((gameResults.gameResults[getId].win / gameResults.gameResults[getId].count * 100).toFixed(0))
+
     updateUserRecord(gameResults.gameResults[getId], getId);
   });
 });
+
+
+
+
 
 
 loseBtn.forEach((elem, idx) => {
-  elem.addEventListener("click", e => {
+  elem.addEventListener('click', e => {
     const button = e.target;
-    const getId = button.id.split("-")[2];
+    const getId = button.id.split('-')[2];
+
     gameResults.gameResults[getId].lose++;
     gameResults.gameResults[getId].count++;
+    gameResults.gameResults[getId].rate = Number((gameResults.gameResults[getId].win / gameResults.gameResults[getId].count * 100).toFixed(0))
+
     updateUserRecord(gameResults.gameResults[getId], getId);
   });
 });
+
+
 
 
 
 const updateUserRecord = (updatedata, id) => {
-  console.dir(updatedata)
+  // console.dir(updatedata);
+  const tmpl = document.querySelector('.player-states');
+
+
+
+
+
+
+
   const playerRecordEl = document.querySelector(`#player-${id}`);
   playerRecordEl.innerHTML = `
-  <th>1</th>
+  <th>0</th>
   <td>
     <a href="#">${updatedata.name}</a>
   </td>
   <td class="count">${updatedata.count}</td>
   <td class="win-count">${updatedata.win}</td>
   <td class="lose-count">${updatedata.lose}</td>
-  <td class="rate-count">${(updatedata.win/updatedata.count*100).toFixed(0)+'%'}</td>
+  <td class="rate-count">${updatedata.rate}</td>
   <td><a id="win-btn-${updatedata.id}" class="win-btn button is-primary">승 추가</a></td>
   <td><a id="lose-btn-${updatedata.id}" class="lose-btn button is-danger">패 추가</a></td>
-</tr>
-  `
-  const winButtonEl = document.querySelector(`#win-btn-${id}`)
-  const loseButtonEl = document.querySelector(`#lose-btn-${id}`)
-  winButtonEl.addEventListener('click', (e)=> {
+  `;
+
+
+
+  const winButtonEl = document.querySelector(`#win-btn-${id}`);
+  const loseButtonEl = document.querySelector(`#lose-btn-${id}`);
+
+
+
+
+  winButtonEl.addEventListener('click', e => {
     const button = e.target;
-    const getId = button.id.split("-")[2];
+    const getId = button.id.split('-')[2];
     gameResults.gameResults[getId].win++;
     gameResults.gameResults[getId].count++;
-    console.log(gameResults.gameResults[getId])
+    gameResults.gameResults[getId].rate = Number((gameResults.gameResults[getId].win / gameResults.gameResults[getId].count * 100).toFixed(0))
+
     updateUserRecord(gameResults.gameResults[getId], getId);
-  })
-  loseButtonEl.addEventListener('click', (e)=> {
+  });
+
+
+
+
+  loseButtonEl.addEventListener('click', e => {
     const button = e.target;
-    const getId = button.id.split("-")[2];
+    const getId = button.id.split('-')[2];
     gameResults.gameResults[getId].lose++;
     gameResults.gameResults[getId].count++;
+    gameResults.gameResults[getId].rate = Number((gameResults.gameResults[getId].win / gameResults.gameResults[getId].count * 100).toFixed(0))
+
     updateUserRecord(gameResults.gameResults[getId], getId);
-  })
+  });
+
+
+
+  if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[1].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[1]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[2].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[2]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[3].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[3]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[4].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[4]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[5].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[5]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[6].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[6]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[7].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[7]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[8].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[8]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[9].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[9]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[10].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[10]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[11].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[11]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[12].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[12]);
+  } else if (Number(playerRecordEl.children[5].innerText > Number(playerRecordEl.parentElement.children[13].children[5].innerText))) {
+    tmpl.insertBefore(playerRecordEl, tmpl.children[13]);
+  }
+
 };
